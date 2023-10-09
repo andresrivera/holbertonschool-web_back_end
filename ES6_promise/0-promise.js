193e1f96b7528a2267fad4a1eaaf43dbc55a9460
andresrivera <andresrivera.web@gmail.com>
2023-10-08 23:17:03 -0500

    Add files

---
+++ ES6_promise/0-promise.js
@@ -0,0 +1,13 @@
+/* eslint-disable */
+function getResponseFromAPI() {
+    return new Promise((resolve, reject) => {
+      if (resolve) {
+        resolve('Work');
+      } else {
+        reject(Error('Broke'));
+      }
+    });
+  }
+  
+  export default getResponseFromAPI;
+      
